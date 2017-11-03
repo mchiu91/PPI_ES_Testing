3c52da5f563d7a294e66f38068d2d37911e9fca9
Hyden Zhang <hyden.zhang@temple.edu>
2017-11-03 13:24:43 -0400

    added dos2unix

--- runL1PPI.sh
+++ runL1PPI.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+#
+
 
 #for task in Emo Soc WM; do
 for task in Soc; do
   #for subj in `cat sublist`; do
   for subj in 100307 100408 100610 101006; do
+    dos2unix L1_${task}_PPI.sh LR $subj
     bash L1_${task}_PPI.sh LR $subj &
     sleep 5s
 
+    dos2unix L1_${task}_PPI.sh RL $subj
     bash L1_${task}_PPI.sh RL $subj
 
   done
--- runL1act.sh
+++ runL1act.sh
@@ -4,8 +4,12 @@
 #for task in WM Soc Emo; do
 for task in WM; do
   for subj in 100307 100408 100610 101006; do
+    dos2unix L1_${task}_Act.sh LR $subj
     bash L1_${task}_Act.sh LR $subj &
+
     sleep 5s
+
+    dos2unix L1_${task}_Act.sh RL $subj
     bash L1_${task}_Act.sh RL $subj
   done
 done
