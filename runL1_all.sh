da6859c331bab3344271281860c6710c057819c9
Hyden Zhang <hyden.zhang@temple.edu>
2017-11-21 16:24:20 -0500

    wrapper for Act, PPI, and social nPPI scripts

---
+++ runL1_all.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+NCORES=28
+
+# Social task
+for subj in `cat sublist`; do
+  for run in RL LR; do
+    #Manages the number of jobs and cores
+    while [ $(ps -ef | grep -v grep | grep L1_Soc_Act.sh | wc -l) -ge $NCORES ]; do
+        sleep 1m
+    done
+    bash L1_Soc_Act.sh $run $subj &
+  done
+done
+
+for subj in `cat sublist`; do
+  for run in RL LR; do
+    #Manages the number of jobs and cores
+    while [ $(ps -ef | grep -v grep | grep L1_Soc_PPI.sh | wc -l) -ge $NCORES ]; do
+        sleep 1m
+    done
+    bash L1_Soc_PPI.sh $run $subj &
+  done
+done
+
+
+# Emotion task
+for subj in `cat sublist`; do
+  for run in RL LR; do
+    #Manages the number of jobs and cores
+    while [ $(ps -ef | grep -v grep | grep L1_Emo_Act.sh | wc -l) -ge $NCORES ]; do
+        sleep 1m
+    done
+    bash L1_Emo_Act.sh $run $subj &
+  done
+done
+
+for subj in `cat sublist`; do
+  for run in RL LR; do
+    #Manages the number of jobs and cores
+    while [ $(ps -ef | grep -v grep | grep L1_Emo_PPI.sh | wc -l) -ge $NCORES ]; do
+        sleep 1m
+    done
+    bash L1_Emo_PPI.sh $run $subj &
+  done
+done
+
+
+# WM task
+for subj in `cat sublist`; do
+  for run in RL LR; do
+    #Manages the number of jobs and cores
+    while [ $(ps -ef | grep -v grep | grep L1_WM_Act.sh | wc -l) -ge $NCORES ]; do
+        sleep 1m
+    done
+    bash L1_WM_Act.sh $run $subj &
+  done
+done
+
+for subj in `cat sublist`; do
+  for run in RL LR; do
+    #Manages the number of jobs and cores
+    while [ $(ps -ef | grep -v grep | grep L1_WM_PPI.sh | wc -l) -ge $NCORES ]; do
+        sleep 1m
+    done
+    bash L1_WM_PPI.sh $run $subj &
+  done
+done
